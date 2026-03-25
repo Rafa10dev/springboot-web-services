b2438329d3ce3977c02de60a4e6d81a809520932
Rafael Felix <raphtiger123@gmail.com>
2026-03-25 18:58:47 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/rafaelfelix/web_services/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.rafaelfelix.web_services.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafaelfelix.web_services.entities.Order;
+import com.rafaelfelix.web_services.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
