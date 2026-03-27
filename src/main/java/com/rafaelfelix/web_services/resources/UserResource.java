0852dead3120b5f97c93decc6b2f105b1fe8f20e
Rafael Felix <raphtiger123@gmail.com>
2026-03-27 20:02:45 -0300

    User insert

--- src/main/java/com/rafaelfelix/web_services/resources/UserResource.java
+++ src/main/java/com/rafaelfelix/web_services/resources/UserResource.java
@@ -1,15 +1,21 @@
 package com.rafaelfelix.web_services.resources;
 
+import java.net.URI;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafaelfelix.web_services.entities.User;
 import com.rafaelfelix.web_services.services.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -31,4 +37,11 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
